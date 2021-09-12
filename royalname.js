17447184e6e7326105ee76a0ea4c2ebf762ac5fd
cjabellera93 <cjabellera@gmail.com>
2021-09-13 02:02:03 +0800

    Initial Creation - Royal Name Generator

---
+++ royalname.js
@@ -0,0 +1,21 @@
+const titles = [ 'Prince', 'Princess', 'King', 'Queen', 'Duke', 'Duchess', 'Marquis', 'High King', 'High Queen'];
+const kingdoms = [ 'Narnia', 'Aerendel', 'the Fairy County', 'the Southern Isles', 'Northern Hemisphere', 'Outer Space'];
+const adjectives = [ 'Adaptable', 'Affectionate', 'Compassionate', 'Zealous', 'Strong'];
+
+const generateName = name => {    
+    const title = getRandomValue(titles);
+    const kingdom = getRandomValue(kingdoms);
+    const adjective = getRandomValue(adjectives);
+    console.log("Your Royal Name is: ");
+    console.log(`${adjective} ${title} ${name} of ${kingdom}`);
+};
+
+const getRandomValue = arr => {
+    return arr[numberGenerator(arr.length)];
+}
+
+const numberGenerator = (num) => {
+    return Math.floor(Math.random() * num);
+}
+
+console.log(generateName('Leo'));
